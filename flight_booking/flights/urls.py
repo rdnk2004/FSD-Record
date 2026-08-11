0c10c04e3fc520869222af181d10c3a4e2d11709
rdnk2004 <rdnikhilkrishna2004@gmail.com>
2026-08-11 11:37:01 +0530

    hh

--- flight_booking/flights/urls.py
+++ flight_booking/flights/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
-urlpatterns ={
-    path('', views.flight_list, name ='flight_list')
-}
+urlpatterns = [
+    path('', views.flight_list, name='flight_list'),
+    path('update/<int:id>/', views.update_flight, name='update_flight'),
+]
--- flight_booking/flights/views.py
+++ flight_booking/flights/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from. models import Flight
 # Create your views here.
 
 def flight_list(request):
     flights = Flight.objects.all()
-    return render (request, 'flights/flights.html', {'flights':flights})
-    
+    return render (request, 'flights.html', {'flights':flights})
+
+def update_flight(request, id):
+    flight = Flight.objects.get(id=id)
+    if request.method == 'POST':
+        flight.name = request.POST['name']
+        flight.destination = request.POST['destination']
+        flight.departure_time = request.POST['departure_time']
+        flight.save()
+        return redirect('flight_list')
+    return render(request, 'update_flight.html', {'flight': flight})
