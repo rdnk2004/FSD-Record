538e8a425e820eb6388cdcb5516ba831f2f8e5c1
rdnk2004 <rdnikhilkrishna2004@gmail.com>
2026-09-03 14:32:52 +0530

    Add ContactForm and implement contact_view for form handling

--- jk-forms/kakaka/forms/views.py
+++ jk-forms/kakaka/forms/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            return redirect('success')
+    else:
+        form = ContactForm()
+    
+    return render(request, 'contact.html', {'form': form})
